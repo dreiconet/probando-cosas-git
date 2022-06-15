3170f03648b4797202ab47b93ecb2fb85a5dfb09
dreiconet <dreiconet@gmail.com>
2022-06-15 18:51:05 -0300

    otro cambio

---
+++ index.js
@@ -0,0 +1,7 @@
+function tablaMultiplicar(tabla, hasta){
+    for (i= 1; i <= hasta; i ++) console.log(tabla, "x", i, "=", tabla * i);
+}
+//for (i = 0; i < 3; i ++) 
+tablaMultiplicar(1,10);
+tablaMultiplicar(5, 10);
+
